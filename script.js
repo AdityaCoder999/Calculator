3eb0300ce65118d5e814ac59b6f66b6612e05632
Aditya SIngh <70559761+AdityaCoder999@users.noreply.github.com>
2022-11-06 14:35:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+
+
+const click1 = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + 1
+}
+const click2 = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + 2
+}
+const click3 = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + 3
+}
+const click4 = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + 4
+}
+const click5 = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + 5
+}
+const click6 = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + 6
+}
+const click7 = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + 7
+}
+const click8 = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + 8
+}
+const click9 = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + 9
+}
+const click0 = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + 0
+}
+const clicka = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + "."
+}
+const clickb = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + "+"
+}
+const clickc = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + "-"
+}
+const clickd = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + "*"
+}
+const clicke = ()=>{
+    const input = document.getElementById('input');
+    input.value = input.value + "/"
+}
+const clickC = ()=>{
+    const input = document.getElementById('input');
+    input.value = "";
+}
+
+const solve = ()=>{
+    const input = document.getElementById('input');
+    const value = input.value;
+    input.value = eval(value)
+
+}
